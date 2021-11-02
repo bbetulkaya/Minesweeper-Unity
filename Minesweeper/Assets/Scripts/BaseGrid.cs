73d3b73d1d3cd64d46cd587e852ab6ece2b0c426
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-11-02 11:28:29 +0300

    + Feat(Grid System) : add show grid in game function

--- Minesweeper/Assets/Scripts/BaseGrid.cs
+++ Minesweeper/Assets/Scripts/BaseGrid.cs
@@ -6,12 +6,15 @@ public class BaseGrid
 {
     public string GridName => _gridName;
     public Sprite GridSprite => _gridSprite;
+    public Transform GridTransform => _gridTransform;
     private string _gridName;
     private Sprite _gridSprite;
+    private Transform _gridTransform;
 
-    public BaseGrid(string name, Sprite sprite)
+    public BaseGrid(string name, Sprite sprite, Transform transform)
     {
         this._gridName = name;
         this._gridSprite = sprite;
+        this._gridTransform = transform;
     }
 }
--- Minesweeper/Assets/Scripts/GridSystem.cs
+++ Minesweeper/Assets/Scripts/GridSystem.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 using UnityEditor;
 public class GridSystem : MonoBehaviour
 {
-
     public int gridSize;
+    public Sprite defaultSprite;
+
+
+    [Header("Sprite Distance")]
+    [Range(0f, 5f)]
+    public float distanceX;
+    [Range(0f, 5f)]
+    public float distanceY;
 
     private int _gridSize => gridSize;
-    private BaseGrid[,] gridArray;
+    private BaseGrid[,] _gridArray;
+    
 
     [ContextMenu("Create Grid")]
     public void CreateGrid()
@@ -17,28 +25,29 @@ public class GridSystem : MonoBehaviour
         parentGridObject.name = "Grid Array";
         CreatePrefab(parentGridObject);
 
-        gridArray = new BaseGrid[_gridSize, _gridSize];
+        _gridArray = new BaseGrid[_gridSize, _gridSize];
 
-        for (int i = 0; i < gridArray.GetLength(0); i++)
+        for (int i = 0; i < _gridArray.GetLength(0); i++)
         {
-            for (int j = 0; j < gridArray.GetLength(1); j++)
+            for (int j = 0; j < _gridArray.GetLength(1); j++)
             {
 
                 GameObject newGridObject = new GameObject();
                 newGridObject.name = "grid_" + i.ToString() + j.ToString();
-                newGridObject.AddComponent<SpriteRenderer>();
+                var spriteRenderer = newGridObject.AddComponent<SpriteRenderer>();
+                spriteRenderer.sprite = defaultSprite;
                 newGridObject.transform.SetParent(parentGridObject.transform);
 
                 PrefabUtility.ApplyPrefabInstance(parentGridObject, InteractionMode.UserAction);
 
-                BaseGrid newGrid = new BaseGrid(newGridObject.name, newGridObject.GetComponent<SpriteRenderer>().sprite);
-                gridArray[i, j] = newGrid;
+
+
+                BaseGrid newGrid = new BaseGrid(newGridObject.name, newGridObject.GetComponent<SpriteRenderer>().sprite, newGridObject.transform);
+                _gridArray[i, j] = newGrid;
             }
         }
 
-        Debug.Log(gridArray[0, 0].GridName);
-        Debug.Log(gridArray[0, 0].GridSprite);
-
+        ShowGrid();
     }
 
     public void CreatePrefab(GameObject gameObject)
@@ -50,4 +59,39 @@ public class GridSystem : MonoBehaviour
         PrefabUtility.SaveAsPrefabAssetAndConnect(gameObject, localPath, InteractionMode.UserAction);
     }
 
+    public void ShowGrid()
+    {
+        float width = (_gridArray.GetLength(0)) * distanceX;
+
+        float currentX = 0;
+        float currentY = 0;
+
+        for (int i = 0; i < _gridArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < _gridArray.GetLength(1); j++)
+            {
+                if (currentX != width)
+                {
+                    Vector3 position = _gridArray[i, j].GridTransform.position;
+                    position = new Vector3(currentX, currentY, 0);
+                    _gridArray[i, j].GridTransform.position = position;
+
+                    currentX = currentX + distanceX;
+
+                }
+                else
+                {
+                    currentX = 0;
+                    currentY = currentY - distanceY;
+
+                    Vector3 position = _gridArray[i, j].GridTransform.position;
+                    position = new Vector3(0, currentY, 0);
+                    _gridArray[i, j].GridTransform.position = position;
+
+                    currentX = currentX + distanceX;
+
+                }
+            }
+        }
+    }
 }
