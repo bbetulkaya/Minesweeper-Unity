e20f26b724713ff6a3c4cb3d3df8d899ba15ca9b
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-11-03 20:42:21 +0300

    + Feat : add game mods scritableobject

---
+++ Minesweeper/Assets/Scripts/ScriptableObjects/GridSettings.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "", menuName = "ScriptableObjects/GridSettings", order = 1)]
+public class GridSettings : ScriptableObject
+{
+    public int gridSize;
+    public Sprite defaultSprite;
+
+
+    [Header("Sprite Distance")]
+    [Range(0f, 5f)]
+    public float distanceX;
+    [Range(0f, 5f)]
+    public float distanceY;
+
+}
