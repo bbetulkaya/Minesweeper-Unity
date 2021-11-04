72fddd82e6a9a32353303584aadb9ecdc20a6377
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-11-04 13:38:15 +0300

    + Feat : add minesweeper sprites data as a scriptableobject

---
+++ Minesweeper/Assets/Scripts/ScriptableObjects/SpriteCollection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "", menuName = "ScriptableObjects/SpriteCollection", order = 1)]
+public class SpriteCollection : ScriptableObject
+{
+    public Sprite defaultSprite;
+    public Sprite mine;
+    public Sprite space;
+    public Sprite number;
+}
